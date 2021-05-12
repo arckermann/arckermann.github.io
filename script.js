7ffad5a091d38ff5c0095abcdd03f7f9d4124df7
arckermann <70674175+arckermann@users.noreply.github.com>
2021-05-12 14:04:11 +0700

    upload template
    iiya hbd

---
+++ script.js
@@ -0,0 +1,12 @@
+var a=prompt("Enter UserName");
+ var b=prompt("Enter date of birth");
+  	if(a== "Caka" && b=="12/05")
+  	{
+  	    window.location.href="start.html";
+  	}
+  	else
+  	{
+  		alert("invalid details");
+  		window.location.href="index.html";
+  	}
+  	
